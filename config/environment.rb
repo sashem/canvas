5c1a008d6b3f16a9d152ca7a2b3812efddf9b176
Sven E. Harfagar Mandiola <mimesis17@gmail.com>
2013-09-10 15:19:57 -0400

    SIN MSJE

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-config.serve_static_assets = true
+
 # Initialize the rails application
 Listr::Application.initialize!
